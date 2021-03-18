3ee04492834e74d0b913e1f475db121dacb6f2a5
MattDEV02 <matteolambertucci3@gmail.com>
2021-03-18 16:09:12 +0100

    set client package

--- src/utils/client.java
+++ src/utils/client.java
@@ -19,18 +19,19 @@ public final class Client extends Object {
     private PrintStream clientOutput = null;
     private String username = "";
 
-    public Client(final InetAddress HOST, final int PORT) {
+    public Client(final InetAddress HOST, final int PORT, final String username) {
         super();
         final String HOSTAddress = HOST.getHostAddress();
-        if(utils.index.isValidSocket(HOSTAddress, PORT)) {
+        final boolean cond = utils.index.isValidSocket(HOSTAddress, PORT);
+        if(cond) {
             try {
                 this.clientSocket = new Socket(HOST, PORT);
+                this.username = username;
                 this.initClientOutput();
                 this.keyboardInput = new Scanner(System.in);
+                this.joinChat();
                 final String serverProcess = utils.index.serverProcess(HOSTAddress, PORT);
-                System.out.println("Client Connected to Server:  | " + serverProcess + " |"); //
-            } catch (final UnknownHostException e){
-                utils.index.handleException(e);
+                System.out.println("Client Connected to Server:  | " + serverProcess + " |");
             } catch(final IOException e) {
                 utils.index.handleException(e);
             }
@@ -44,6 +45,8 @@ public final class Client extends Object {
         if(c != null)  {
             this.setClientSocket(c.clientSocket);
             this.setUsername(c.username);
+            this.setClientOutput(c.clientOutput);
+            this.setKeyboardInput(c.keyboardInput);
         } else {
             final String className = this.getClass().toString();
             utils.index.handleCopy_Construct_ERR(className);
@@ -83,40 +86,25 @@ public final class Client extends Object {
         }
     }
 
-    public final void comunication() {
+    public final void joinChat() {
+        final String keyWord = index.strJoin;
+        this.clientOutput.println(keyWord);
+    }
+
+    public final void send(String msg) {
         try {
-            new clientThread(this.clientSocket).start(); // start new Thread
-            this.enterUsername();
-            this.sendMsg();
-            this.close();
-        } catch(final IOException e) {
+            final String keyWord = index.strLeave;
+            if(msg.equals(keyWord)) {
+                this.clientOutput.println(keyWord);
+                this.close();
+            }
+            msg =  utils.index.capitalize(msg);
+            final String now = utils.index.getDate();
+            final String s = (this.username + ": " + msg + now);
+            this.clientOutput.println(s);
+        } catch (IOException e) {
             utils.index.handleException(e);
         }
-    }
-
-    public final void enterUsername() {
-        System.out.print("Enter your Username: ");
-        String username = this.keyboardInput.nextLine();
-        if(username.length() <= 0)
-            username = utils.index.randomString(5);
-        this.setUsername(username);
-        this.clientOutput.println(utils.index.strJoin);
-    }
-
-    public final void sendMsg() {
-        System.out.println("\n" + this.username + ", enter your messages (leave to exit) :  \n");
-        final String keyWord = utils.index.strLeave;
-        do {
-            String msg = this.keyboardInput.nextLine();
-            if(msg.length() <= 0)
-                msg = utils.index.randomString(5);
-            else if(msg.equals(keyWord)) {
-                this.clientOutput.println(keyWord);
-                break;
-            }
-            final String s = (this.username + ": " + msg + utils.index.getDate());
-            this.clientOutput.println(s);
-        } while (keyboardInput.hasNextLine());
     }
 
     public final synchronized void close() throws IOException {
@@ -136,12 +124,5 @@ public final class Client extends Object {
         } catch (final IOException e) {
             utils.index.handleException(e);
         }
-    }
-
-    public final static void main(final String[] args) {
-        final InetAddress HOST = utils.index.HOST;
-        final int PORT = utils.index.PORT;
-        final Client c = new Client(HOST, PORT);
-        c.comunication();
     }
 }
